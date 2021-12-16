c9a78cc101f586fa2583ab2755b21ebdcdc7b3cc
Jake Mulford <jakemulf@gmail.com>
2021-12-16 15:53:27 -0500

    functionalized the read rss method

--- README.md
+++ README.md
@@ -15,3 +15,11 @@ This app pulls data from an RSS feed, performs sentiment analysis on the data, a
 * [`read_rss_default_analysis.py`](read_rss_default_analysis.py) - An RSS reader that uses NLKT's default sentiment analysis.
 * [`read_rss_custom_analysis.py`](read_rss_custom_analysis.py) - An RSS reader that uses a user-defined sentiment analysis.
 * [`queries.py`](queries.py) - Queries to run in Deephaven for extra analysis on the data.
+
+## High level overview
+
+This app pulls RSS data from a specified RSS feed using Python's [feedparser](https://pypi.org/project/feedparser/) package. When data is pulled, sentiment analysis is performed on attributes of the RSS data and stored in a Deephaven table. Deephaven table operations are then used to further analyze the data.
+
+This app shows two examples of sentiment analysis, one using the default analyzer from [NLTK](https://www.nltk.org/), and one using an analyzer trained from NLTK's built in data.
+
+By default, this app pulls from a Reddit RSS feed and performs sentiment analysis on the title of each post. The feed and attributes to analyze are customizable, so feel free to use this app to look at any RSS feed!
--- python-scripts/read_rss_default_analysis.py
+++ python-scripts/read_rss_default_analysis.py
@@ -1,19 +1,25 @@
 """
 read_rss_default_analysis.py
 
-A simple rss reader in Python that does sentiment analysis using the NLKT default sentiment analysis, and stores the results in Deephaven
+A simple rss reader in Python that does sentiment analysis using the NLKT default sentiment analysis, and stores the results in Deephaven.
 """
 from deephaven import DynamicTableWriter, Types as dht
-
-import feedparser
 
 import nltk
 nltk.download('vader_lexicon')
 from nltk.sentiment import SentimentIntensityAnalyzer
 
-import time
-import threading
-from datetime import datetime
+def build_default_sia_classifier_func(classifier):
+    def a(strn):
+        sentiment = classifier.polarity_scores(strn)
+        return sentiment["pos"], sentiment["neu"], sentiment["neg"], sentiment["compound"], strn
+    return a
+
+rss_feed_url = "https://www.reddit.com/r/wallstreetbets/new/.rss"
+rss_attributes = [
+    "title"
+]
+classifier = build_default_sia_classifier_func(SentimentIntensityAnalyzer())
 
 column_names = [
     "Positive",
@@ -29,45 +35,8 @@ column_types = [
     dht.double,
     dht.string
 ]
-
 built_in_sia_writer = DynamicTableWriter(column_names, column_types)
 built_in_sia = built_in_sia_writer.getTable()
 
-sia = SentimentIntensityAnalyzer()
-
-def read_rss(rss_feed_url, rss_attributes, sleep_time=5):
-    last_updated = None
-
-    while True:
-        feed = feedparser.parse(rss_feed_url)
-
-        i = 0
-        while i < len(feed.entries):
-            entry = feed.entries[i]
-
-            if not (last_updated is None) and datetime.fromisoformat(entry["updated"]) <= datetime.fromisoformat(last_updated):
-                break
-
-            for attribute in rss_attributes:
-                sentiment = sia.polarity_scores(entry[attribute])
-                built_in_sia_writer.logRow(
-                    sentiment["pos"],
-                    sentiment["neu"],
-                    sentiment["neg"],
-                    sentiment["compound"],
-                    entry[attribute]
-                )
-
-            i += 1
-
-        if i == 0: #Feed hasn't been updated, sleep
-            time.sleep(sleep_time)
-        else: #Otherwise set last updated time to the newest item in the feed
-            last_updated = feed.entries[0]["updated"]
-
-rss_feed_url = "https://www.reddit.com/r/wallstreetbets/new/.rss"
-rss_attributes = [
-    "title"
-]
-thread = threading.Thread(target=read_rss, args=[rss_feed_url, rss_attributes])
+thread = threading.Thread(target=read_rss, args=[rss_feed_url, rss_attributes, classifier, built_in_sia_writer])
 thread.start()
