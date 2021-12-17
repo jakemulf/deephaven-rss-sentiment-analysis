f0339113beeb000f4fec18f387c2a8c21ca721b6
Jake Mulford <jakemulf@gmail.com>
2021-12-17 15:29:55 -0500

    Generalize rss (#5)
    * generalized the rss reader
    
    * updated build workflow to reflect new tables
    
    * grammar fix
    
    * extra comma

--- app.d/read_rss_default_analysis.py
+++ app.d/read_rss_default_analysis.py
@@ -1,10 +1,11 @@
 """
 read_rss_default_analysis.py
 
-A simple rss reader in Python that does sentiment analysis using the NLKT default sentiment analysis, and stores the results in Deephaven.
-"""
-from deephaven import DynamicTableWriter, Types as dht
+An RSS reader in Python that does sentiment analysis using the NLKT default sentiment analysis, and stores the results in Deephaven.
 
+This file is meant to run through Deephaven's Application Mode as part of several Python scripts. Because of this, some
+variables may not be defined in here, but instead in helper_functions.py.
+"""
 import nltk
 nltk.download('vader_lexicon')
 from nltk.sentiment import SentimentIntensityAnalyzer
@@ -26,17 +27,33 @@ column_names = [
     "Neutral",
     "Negative",
     "Compound",
-    "Sentence"
+    "Sentence",
+    "Datetime"
 ]
 column_types = [
     dht.double,
     dht.double,
     dht.double,
     dht.double,
-    dht.string
+    dht.string,
+    dht.datetime
 ]
-built_in_sia_writer = DynamicTableWriter(column_names, column_types)
-built_in_sia = built_in_sia_writer.getTable()
+built_in_sia_wsb_writer = DynamicTableWriter(column_names, column_types)
+built_in_sia_wsb = built_in_sia_wsb_writer.getTable()
 
-thread = threading.Thread(target=read_rss, args=[rss_feed_url, rss_attributes, classifier, built_in_sia_writer])
-thread.start()
+thread_wsb = threading.Thread(target=read_rss, args=[rss_feed_url, rss_attributes, classifier, built_in_sia_wsb_writer, datetime_converter_reddit])
+thread_wsb.start()
+
+rss_feed_url = "https://www.reddit.com/r/all/new/.rss"
+built_in_sia_all_writer = DynamicTableWriter(column_names, column_types)
+built_in_sia_all = built_in_sia_all_writer.getTable()
+
+thread_all = threading.Thread(target=read_rss, args=[rss_feed_url, rss_attributes, classifier, built_in_sia_all_writer, datetime_converter_reddit, 1])
+thread_all.start()
+
+rss_feed_url = "https://hnrss.org/newest"
+built_in_sia_hackernews_writer = DynamicTableWriter(column_names, column_types)
+built_in_sia_hackernews = built_in_sia_hackernews_writer.getTable()
+
+thread_hackernews = threading.Thread(target=read_rss, args=[rss_feed_url, rss_attributes, classifier, built_in_sia_hackernews_writer, datetime_converter_hackernews, 60])
+thread_hackernews.start()
