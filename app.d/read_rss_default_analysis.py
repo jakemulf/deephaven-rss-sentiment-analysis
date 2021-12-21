69940e83e121cb30cd5ad5e8b0498570e484ac9f
Jake Mulford <jakemulf@gmail.com>
2021-12-21 11:08:52 -0500

    cleaned up code

--- app.d/read_rss_default_analysis.py
+++ app.d/read_rss_default_analysis.py
@@ -6,8 +6,6 @@ An RSS reader in Python that does sentiment analysis using the NLKT default sent
 This file is meant to run through Deephaven's Application Mode as part of several Python scripts. Because of this, some
 variables may not be defined in here, but instead in helper_functions.py or read_rss.py.
 """
-from deephaven import as_list
-
 import nltk
 nltk.download('vader_lexicon')
 from nltk.sentiment import SentimentIntensityAnalyzer
@@ -15,7 +13,7 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 def build_default_sia_classifier_func(classifier):
     def a(strn):
         sentiment = classifier.polarity_scores(strn)
-        return as_list([sentiment["pos"], sentiment["neu"], sentiment["neg"], sentiment["compound"]])
+        return [sentiment["pos"], sentiment["neu"], sentiment["neg"], sentiment["compound"]]
     return a
 
 rss_attributes = [
@@ -33,17 +31,5 @@ rss_feed_url = "https://hnrss.org/newest"
 built_in_sia_hackernews = read_rss(rss_feed_url, rss_attributes, datetime_converter_hackernews, sleep_time=60)
 
 built_in_sia_wsb = built_in_sia_wsb.update("Sentiment = classifier(Sentence)")
-#    .update("Positive = Sentiment[0]")\
-#    .update("Neutral = Sentiment[1]")\
-#    .update("Negative = Sentiment[2]")\
-#    .update("Compound = Sentiment[3]")
 built_in_sia_all = built_in_sia_all.update("Sentiment = classifier(Sentence)")
-#    .update("Positive = Sentiment[0]")\
-#    .update("Neutral = Sentiment[1]")\
-#    .update("Negative = Sentiment[2]")\
-#    .update("Compound = Sentiment[3]")
 built_in_sia_hackernews = built_in_sia_hackernews.update("Sentiment = classifier(Sentence)")
-#    .update("Positive = Sentiment[0]")\
-#    .update("Neutral = Sentiment[1]")\
-#    .update("Negative = Sentiment[2]")\
-#    .update("Compound = Sentiment[3]")
